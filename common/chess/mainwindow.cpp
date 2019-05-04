129a1fb99521f0099eae33fcf0c62139810c0b7c
Jan Holásek <xholas09@users.noreply.github.com>
2019-05-04 22:22:58 +0200

    Add 'on click' actions to menu options, add close action to tabs, add basic behavior to some actions

--- common/chess/mainwindow.cpp
+++ common/chess/mainwindow.cpp
@@ -12,3 +12,46 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_actionNewGame_triggered()
+{
+    addTab();
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+
+}
+
+void MainWindow::on_actionSaveAs_triggered()
+{
+
+}
+
+void MainWindow::on_actionClose_triggered()
+{
+    close();
+}
+
+void MainWindow::on_tabWidget_tabCloseRequested(int index)
+{
+    closeTab(index);
+}
+
+void MainWindow::addTab()
+{
+    QWidget* newGame = new QWidget;
+
+    ui->tabWidget->addTab(newGame, "*New Game");
+    ui->tabWidget->setCurrentWidget(newGame);
+}
+
+void MainWindow::closeTab(int index)
+{
+    ui->tabWidget->removeTab(index);
+}
--- common/chess/mainwindow.h
+++ common/chess/mainwindow.h
@@ -15,8 +15,25 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionNewGame_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionSaveAs_triggered();
+
+    void on_actionClose_triggered();
+
+    void on_tabWidget_tabCloseRequested(int index);
+
 private:
     Ui::MainWindow *ui;
+
+    void addTab();
+
+    void closeTab(int index);
 };
 
 #endif // MAINWINDOW_H
