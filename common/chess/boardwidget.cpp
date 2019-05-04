b976591a2ad06a16fe70edb537b121aa02874143
Jan Holásek <xholas09@users.noreply.github.com>
2019-05-05 00:41:26 +0200

    Add figures, show dummy figures on chess-board

--- common/chess/boardwidget.cpp
+++ common/chess/boardwidget.cpp
@@ -6,6 +6,38 @@ BoardWidget::BoardWidget(QWidget *parent) :
     ui(new Ui::BoardWidget)
 {
     ui->setupUi(this);
+
+    scale = static_cast<double>(ui->boardLabel->width()) / 1000;
+
+    figures = figureBuffer(8, std::vector<QLabel*>(8));
+
+    // White figures
+    addFigure(FigureType::rogue,    TeamColor::white, 0, 0);
+    addFigure(FigureType::knight,   TeamColor::white, 1, 0);
+    addFigure(FigureType::bishop,   TeamColor::white, 2, 0);
+    addFigure(FigureType::queen,    TeamColor::white, 3, 0);
+    addFigure(FigureType::king,     TeamColor::white, 4, 0);
+    addFigure(FigureType::bishop,   TeamColor::white, 5, 0);
+    addFigure(FigureType::knight,   TeamColor::white, 6, 0);
+    addFigure(FigureType::rogue,    TeamColor::white, 7, 0);
+    // White pawns
+    for (int x = 0; x < 8; x++) {
+        addFigure(FigureType::pawn, TeamColor::white, x, 1);
+    }
+
+    // Black figures
+    addFigure(FigureType::rogue,    TeamColor::black, 0, 7);
+    addFigure(FigureType::knight,   TeamColor::black, 1, 7);
+    addFigure(FigureType::bishop,   TeamColor::black, 2, 7);
+    addFigure(FigureType::queen,    TeamColor::black, 3, 7);
+    addFigure(FigureType::king,     TeamColor::black, 4, 7);
+    addFigure(FigureType::bishop,   TeamColor::black, 5, 7);
+    addFigure(FigureType::knight,   TeamColor::black, 6, 7);
+    addFigure(FigureType::rogue,    TeamColor::black, 7, 7);
+    // Black pawns
+    for (int x = 0; x < 8; x++) {
+        addFigure(FigureType::pawn, TeamColor::black, x, 6);
+    }
 }
 
 BoardWidget::~BoardWidget()
@@ -23,18 +55,91 @@ void BoardWidget::resizeEvent(QResizeEvent* event)
     int newBoardSize = event->size().width() > event->size().height() ? event->size().height() : event->size().width();
     ui->boardLabel->resize(newBoardSize, newBoardSize);
 
-    scale = static_cast<double>(ui->boardLabel->width()) / 1000;
+    scale = static_cast<double>(ui->boardLabel->width()) / boardSize;
 
-    /*
+    int newFigureSize = static_cast<int>( figureSize * scale );
+
     for (int x = 0; x < 8; x++) {
         for (int y = 0; y < 8; y++) {
-            if (labels[x][y] != nullptr)
+            QLabel* figure = figures[x][y];
+            if (figure != nullptr)
             {
-                labels[x][y]->move(getFigureImgPosition(x, y));
+                figure->move(getFigureImgPosition(x, y));
+                figure->resize(newFigureSize, newFigureSize);
+
+                // TODO: img has to be reloaded from resources
+                figure->setPixmap(figure->pixmap()->scaled(newFigureSize, newFigureSize, Qt::KeepAspectRatio));
             }
         }
     }
-    */
 
     //QMessageBox::information(this, nullptr, "label size: " + QString::number(ui->boardLabel->width()) + "\nevent size: " + QString::number(event->size().width()), QMessageBox::Ok);
+}
+
+void BoardWidget::addFigure(FigureType type, TeamColor team, int posX, int posY)
+{
+    QLabel* newFigure = new QLabel(this);
+    QPixmap img;
+
+    if (team == TeamColor::white)
+    {
+        switch (type) {
+        case FigureType::pawn:
+            img = QPixmap(":img/pawn_white.png");
+            break;
+        case FigureType::rogue:
+            img = QPixmap(":img/rogue_white.png");
+            break;
+        case FigureType::knight:
+            img = QPixmap(":img/knight_white.png");
+            break;
+        case FigureType::bishop:
+            img = QPixmap(":img/bishop_white.png");
+            break;
+        case FigureType::queen:
+            img = QPixmap(":img/queen_white.png");
+            break;
+        case FigureType::king:
+            img = QPixmap(":img/king_white.png");
+            break;
+        }
+    }
+    else
+    {
+        switch (type) {
+        case FigureType::pawn:
+            img = QPixmap(":img/pawn_black.png");
+            break;
+        case FigureType::rogue:
+            img = QPixmap(":img/rogue_black.png");
+            break;
+        case FigureType::knight:
+            img = QPixmap(":img/knight_black.png");
+            break;
+        case FigureType::bishop:
+            img = QPixmap(":img/bishop_black.png");
+            break;
+        case FigureType::queen:
+            img = QPixmap(":img/queen_black.png");
+            break;
+        case FigureType::king:
+            img = QPixmap(":img/king_black.png");
+            break;
+        }
+    }
+
+    int newFigureSize = static_cast<int>( figureSize * scale );
+
+    newFigure->setPixmap(img.scaled(newFigureSize, newFigureSize, Qt::KeepAspectRatio));
+    newFigure->move(getFigureImgPosition(posX, posY));
+
+    figures[posX][posY] = newFigure;
+}
+
+QPoint BoardWidget::getFigureImgPosition(int x, int y)
+{
+    int posX = static_cast<int>( ((boardSquareSize * x) + boardBorderSize) * scale );
+    int posY = static_cast<int>( ((boardSquareSize * (7 - y)) + boardBorderSize) * scale ); // 7 - y for reverse order of rows (7 on top down to 0 on bottom of chess-board)
+
+    return QPoint (posX, posY);
 }
--- common/chess/boardwidget.h
+++ common/chess/boardwidget.h
@@ -2,6 +2,7 @@
 #define BOARDWIDGET_H
 
 #include <QWidget>
+#include <QLabel>
 #include <QResizeEvent>
 
 namespace Ui {
@@ -13,6 +14,21 @@ class BoardWidget : public QWidget
     Q_OBJECT
 
 public:
+    enum class TeamColor
+    {
+        white,
+        black
+    };
+    enum class FigureType
+    {
+        pawn,
+        rogue,
+        knight,
+        bishop,
+        queen,
+        king
+    };
+
     explicit BoardWidget(QWidget *parent = nullptr);
     ~BoardWidget();
 
@@ -21,10 +37,22 @@ public:
 private:
     Ui::BoardWidget *ui;
 
+    double boardSize = 1000;
+    double boardBorderSize = 47 + 3; // border + offset for position correction
+    double boardSquareSize = 113.25;
+
+    double figureSize = 110;
+
     double scale;
+    //QLabel* labels[8][8];
+
+    typedef std::vector<std::vector<QLabel*>> figureBuffer;
+    figureBuffer figures;
 
     void resizeEvent(QResizeEvent* event);
 
+    void addFigure(FigureType type, TeamColor team, int posX, int posY);
+    QPoint getFigureImgPosition(int x, int y);
 };
 
 #endif // BOARDWIDGET_H
